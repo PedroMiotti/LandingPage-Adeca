d171b4cf1d37faa63d507825242842aa3b3f0522
Pedro Miotti <pedromiotti7@gmail.com>
2020-10-21 15:57:07 -0300

    Fixed resposive && Added footer && Added modal

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ $(document).ready(function() {
 
   })
 
-
   // Fixed navbar on Scroll
   let header = $('#navbar');
   let navbuttons = $('.navbuttons');
@@ -43,10 +42,93 @@ $(document).ready(function() {
     }
   }
 
+
+  // Show modal on scroll
+  function modalOnScroll() {
+    let modal = $("#modal-conteudo");
+    let overlay = $('#overlay-modal');
+    let body = $('body');
+    
+    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+    
+    if ($(document).scrollTop() > 700 && modal.attr("displayed") === "false") {
+      modal.attr("displayed", "true");
+      modal.removeClass('hide-modal');
+      modal.addClass('modal-conteudo-container');
+
+      overlay.removeClass('hide-overlay-modal');
+      overlay.addClass('overlay-modal')
+      overlay.css('top', `${scrollY}`)
+
+      body.css('position', 'fixed');
+      body.css('top', `-${scrollY}`)
+      
+    }
+  }
+
+  const closeModal = () => {
+    let body = $('body');
+    let modal = $("#modal-conteudo");
+    let overlay = $('#overlay-modal');
+
+    const scrollY = body.css('top');
+
+    body.css('position', '');
+    body.css('top', '');
+    
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+    modal.removeClass('modal-conteudo-container');
+    modal.addClass('hide-modal');
+
+    overlay.removeClass('overlay-modal')
+    overlay.addClass('hide-overlay-modal');
+
+  }
+
+  // Close modal button
+  let closeBttModal = $('#close-modal-btt');
+  closeBttModal.click(() => {
+    closeModal();
+  })
+
+
+  window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+  });
+
   window.onscroll = () => {
     navbarOnScroll();
+    modalOnScroll();
   }
+
+
+
+
+
+  // toggle input inside modal
+  let whatsChecked = $('#whats-radio');
+  let emailChecked = $('#email-radio');
+
+  let emailInput = $('#email-input');
+  emailInput.addClass('hide-form-input')
+
+  let telInput = $('#tel-input');
+
+  // let valorRadio = $('input[name=receber-por]:checked').val();
   
+  emailChecked.click(() => {
+    telInput.toggleClass('hide-form-input')
+    emailInput.removeClass('hide-form-input')
+  })
+  
+  whatsChecked.click(() => {
+      emailInput.toggleClass('hide-form-input')
+      telInput.removeClass('hide-form-input')
+  })
+
+
+
   
     // Servicos card flip animation
     $('.button-flip-control1').click(function() {
@@ -99,7 +181,8 @@ $(document).ready(function() {
               slidesToShow: 1,
               slidesToScroll: 1,
               infinite: true,
-              dots: false
+              dots: false,
+              arrows : false,
             }
           }
         ]
